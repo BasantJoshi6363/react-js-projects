39a0897a756892521e01b06026ea8f68196fbf4d
BasantJoshi6363 <basantjoshi6363@gmail.com>
2024-07-22 15:03:31 +0545

    styling changes

--- src/bg_changer/Bg_Changer.jsx
+++ src/bg_changer/Bg_Changer.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
+import "./index.css";
 
 const BgChanger = () => {
     const [color,setColor] = useState("")
 
     const buttons = ["yellow", "green", "red", "blue", "purple", "pink", "white", "olive"];
     return (
+
         <div className='main-inner'>
+            <div className="inner-content">
             {buttons.map((value, i) => {
                 return <button onClick={()=>{
                     setColor(value)
                     document.body.style.backgroundColor = value;
                 }} key="i" style={{ backgroundColor: `${value}`, cursor: "pointer",color:"black" }}>{value}</button>
             })}
+            </div>
+            
         </div>
     )
 }
